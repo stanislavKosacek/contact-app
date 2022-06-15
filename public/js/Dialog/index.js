f8c638529574a114bdc346ff211a9830bf43aca5
Stanislav Kosáček <kosacek@brilo.cz>
2022-06-15 23:43:45 +0200

    fix xss

--- public/js/Dialog/index.js
+++ public/js/Dialog/index.js
@@ -1,33 +1,35 @@
 export const Dialog = (props) => {
-    const dialogElm = document.createElement('dialog');
-    dialogElm.innerHTML = `
-        <p>${props.note}</p>
+  const dialogElm = document.createElement("dialog");
+  const pElm = document.createElement("p");
+  pElm.textContent = props.note;
+  dialogElm.appendChild(pElm);
+
+  dialogElm.innerHTML += `
         <form method="dialog">
             <button value="cancel">${props.cancelText}</button>
         </form>`;
 
-    dialogElm.addEventListener('close', () => {
-        dialogElm.remove();
-    })
+  dialogElm.addEventListener("close", () => {
+    dialogElm.remove();
+  });
 
-    return dialogElm;
-}
+  return dialogElm;
+};
 
 export const dialogClickHandler = (e) => {
-    if (e.target.tagName !== 'DIALOG') {
-        return;
-    }
+  if (e.target.tagName !== "DIALOG") {
+    return;
+  }
 
-    const rect = e.target.getBoundingClientRect();
+  const rect = e.target.getBoundingClientRect();
 
-    const clickedInDialog = (
-        rect.top <= e.clientY &&
-        e.clientY <= rect.top + rect.height &&
-        rect.left <= e.clientX &&
-        e.clientX <= rect.left + rect.width
-    );
+  const clickedInDialog =
+    rect.top <= e.clientY &&
+    e.clientY <= rect.top + rect.height &&
+    rect.left <= e.clientX &&
+    e.clientX <= rect.left + rect.width;
 
-    if (clickedInDialog === false) {
-        e.target.close();
-    }
-}
+  if (clickedInDialog === false) {
+    e.target.close();
+  }
+};
